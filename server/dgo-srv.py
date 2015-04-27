bf449af799d5c4ab7662f47744abd58fa7efa1dc
Adam Mayer <phooky@gmail.com>
2015-04-27 19:27:43 -0400

    refactored mightily

--- server/dgo-srv.py
+++ server/dgo-srv.py
@@ -7,6 +7,8 @@ import time
 VERSION=1
 PORT=3904
 
+from enum import Enum
+
 # Summary of interactions:
 # C1) Hello, I am X running Y.
 # S1) Hi, X. You are supported.
@@ -25,93 +27,178 @@ PORT=3904
 class NotIdentified(Exception):
     pass
 
-active_clients = []
-active_offers = []
-active_games = []
+# Map of user ids to users
+users = {}
+# Map of game ids to games
+games = {}
+# List of outstanding offers
+offers = []
 
-def find_user(handle):
-    for client in active_clients:
-        if client.handle == handle:
-            return client
-    return None
+class UserState(Enum):
+    login = 0
+    lobby = 1
+    playing = 2
+    watching = 3
+
+class User:
+    def __init__(self,handle,addr,protocol):
+        self.handle = handle
+        self.addr = addr
+        self.status = UserState.login
+        self.protocol = protocol
+        self.desc = { 'id':id(self), 'handle':self.handle }
+        users[id(self)] = self
+        self.game = None
+
+    def update_users_games(self):
+        self.protocol.send_json( {
+            't':'users_games',
+            'users':[x.desc for x in users.values()],
+            'games':[x.desc for x in games.values()],
+            })
 
 class Offer:
+    def find(by, to):
+        for offer in offers:
+            if offer.by == by and offer.to == to:
+                return offer
+        return None
+
     def __init__(self,by,to):
+        "Create an offer and send proposal message to target"
         self.by = by
         self.to = to
+        offers.append(self)
+        to.protocol.send_json( self.make_update('new') )
+
+    def make_update(self,status):
+        return { 't':'offer', 'by':self.by.desc, 'to':self.to.desc, 'status':status }
+
+    def resolve(self, status):
+        "Send offer resolution to both parties and remove from offers"
+        to.protocol.send_json( self.make_update(status) )
+        by.protocol.send_json( self.make_update(status) )
+        offers.remove(self)
+
+    def reject(self):
+        self.resolve('rejected')
+
+    def cancel(self):
+        self.resolve('cancelled')
+
+    def accept(self):
+        self.resolve('accepted')
+        # START GAME HERE
+
+class Game:
+    def __init__(self,white,black):
+        self.white = white
+        self.black = black
+        self.start = time.time()
+        games[id(self)] = self
+        self.moves = []
+        self.watchers = set()
+        self.subscribe(black)
+        self.subscribe(white)
+
+    def subscribe(self,watcher):
+        if watcher not in self.watchers:
+            watchers.add(watcher)
+            watcher.protocol.send_json( { 't':'game', 'dsc':self.desc, 'moves':moves } )
+
+    def unsubscribe(self,watcher):
+        watchers.remove(watcher)
+
+    @property
+    def desc(self):
+        return { 'id':id(self), 'white':self.white.desc, 'black':self.black.desc, 'start':self.start }
+
+    def play_move(self,by,move):
+        if (len(moves) % 2)  == 0:
+            if by != self.black:
+                raise Exception()
+            elif by != self.white:
+                raise Exception()
+            else:
+                raise Exception()
+        self.moves.append(move)
+        update = { 't':'game_upd', 'id':id(self), 'by':by.desc, 'move':move }
+        for watcher in self.watchers:
+            watcher.protocol.send_json(update)
+
 
 class DiamondGoProtocol(WebSocketServerProtocol):
 
     def send_json(self, obj):
         self.sendMessage(json.dumps(obj,ensure_ascii=False).encode('utf-8'),isBinary=False)
 
-    def do_offers(self,command):
-        o = { 
-            't' : 'offers',
-            'users' : [x.by.identity for x in active_offers if x.to == self]
-            }
-        self.send_json(o)
-
-    def do_users(self,command):
-        o = { 
-            't' : 'user_list',
-            'users' : [x.identity for x in active_clients]
-            }
-        self.send_json(o)
-
-    def do_propose(self,command):
-        global active_offers
-        user = find_user(command['user']['handle'])
-        print("Propose: {0} proposes game to {1}".format(self.identity['handle'],command['user']['handle']))
-        if not user:
-            return
-        # remove other offers from this user
-        updates = set([x.to for x in active_offers if x.by == self])
-        updates.add(self)
-        updates.add(user)
-        active_offers = [x for x in active_offers if x.by != self]
-        # add new offer
-        offer = Offer(self,user)
-        active_offers.append(offer)
-        # send updated offer list to all changed parties
-        for u in updates:
-            u.do_offers(None)
-        print("Proposed: {0} proposes game to {1}".format(self.identity['handle'],user.identity['handle']))
-
-    def do_hello(self,command):
+    def do_hello_req(self,command):
+        "Basic login"
         #TODO: disallow duplicate handles
-        self.handle = command['handle']
-        self.identity = { 'handle': self.handle, 'addr': self.addr } 
+        self.user = User(command['handle'],self.addr,self)
         self.send_json( {
-            't' : 'hello_ack',
-            'id' : self.identity,
+            't' : 'hello',
+            'id' : id(self.user),
             } )
-        print("Login: {0} at {1}".format(self.handle, time.asctime()))
-        # update other clients
-        for client in active_clients:
-            if client != self:
-                client.do_users(None)
+        print("Login: {0} at {1}".format(self.user.handle, time.asctime()))
+        # update self and others
+        for user in users.values():
+            user.update_users_games()
 
+    def do_users_games_req(self,command):
+        self.user.update_users_games()
+
+    def do_make_offer(self,command):
+        to = users[command['to']]
+        by = self.user
+        if Offer.find(by,to):
+            raise Exception()
+        offer = Offer(by,to)
+
+    def do_offer_rsp(self,command):
+        to = users[command['to']]
+        by = users[command['by']]
+        status = command['status']
+        Offer.find(by,to).resolve(status)
+
+    def do_watch_game(self,command):
+        pass
+
+    def do_leave_game(self,command):
+        pass
+
+    def do_move(self,command):
+        pass
 
     handlers = {
-            'hello' : do_hello,
-            'users' : do_users,
-            'propose_game' : do_propose,
+            'hello_req' : do_hello_req,
+            'users_games_req' : do_users_games_req,
+            'make_offer' : do_make_offer,
+            'offer_rsp' : do_offer_rsp,
+            'watch_game' : do_watch_game,
+            'leave_game' : do_leave_game,
+            'move' : do_move,
             }
 
     def onConnect(self,info):
-        self.identity = None
+        self.user = None
         self.addr = info.peer
 
     def onOpen(self):
-        active_clients.append(self)
+        pass
 
     def onClose(self,wasClean,code,reason):
         try:
-            active_clients.remove(self)
-        except:
+            print("Close..")
+            if self.user:
+                print("Leaves: {0} at {1} for {2}".format(self.user.handle,time.asctime(),reason))
+                del users[id(self.user)]
+                for user in users.values():
+                    user.update_users_games()
+            self.user = None
+        except Exception as e:
+            print(e)
             pass
-        print("Closed: {0} at {1} for {2}".format(self.handle,time.asctime(),reason))
     
     def onMessage(self, payload, isBinary):
         try:
@@ -121,9 +208,6 @@ class DiamondGoProtocol(WebSocketServerProtocol):
         except Exception as e:
             print(e)
 
-    def check_ident(self):
-        if not self.identity:
-            raise NotIdentified()
 
 if __name__ == '__main__':
     import asyncio
---
+++ server/protocol.md
@@ -0,0 +1,60 @@
+Protocol Outline
+
+Every message is a JSON object.
+
+'t' field is type.
+
+USERID, GAMEID are numeric identifiers
+
+GAMEDSC is a game descriptor of the form
+{ "id":GAMEID, "white":USERDSC, "black":USERDSC, "start":TIME }
+
+USERDSC is a user descriptor of the form
+{ "id":USERID, "handle":HANDLE }
+
+
+=>   Hello request (client->server)
+{ "t":"hello_req", "handle":"HANDLE", (auth) }
+
+Hello response (server->client)
+{ "t":"hello", "id": USERID }
+
+
+=>   Request users and games (client->server)
+{ "t":"users_games_req" }
+
+Users and games (server->client)
+{ "t":"users_games", "users":[ USERDSC, USERDSC, ... ], "games":[ GAMEDSC, GAMEDSC, .... ] }
+
+Don't bother with separate updates for now; resend the entire list to every connected player on update.
+
+
+=>   Make offer (client->server)
+{ "t":"make_offer", "to":USERID }
+
+Notify of offer (server->client)
+{ "t":"offer", "by":USERDSC, "to":USERDSC, "status":OFFER_STATUS }
+where OFFER_STATUS is one of "new", "accepted", "rejected", "cancelled"
+
+=>   Accept/Refuse offer (client->server)
+{ "t":"offer_rsp", "by":USERDSC, "to":USERDSC, "status":OFFER_STATUS }
+where OFFER_STATUS is one of "accepted","rejected"
+
+
+=>   Follow a game (client->server)
+{ "t":"watch_game", "id":GAMEID }
+
+=>   Leave/stop following a game (client->server)
+{ "t":"leave_game", "id":GAMEID }
+
+
+Send a game to a client
+{ "t":"game", "dsc":GAMEDSC, "moves":[ MOVE, MOVE, ... ] }
+
+Update a game with a move
+{ "t":"game_upd", id:GAMEID, "move":MOVE }
+
+=>   Make a move (client->server)
+{ "t":"move", id:GAMEID, "move":MOVE }
+
+A move is currently an array of three digits indicating the layer, x, and y of the move.
